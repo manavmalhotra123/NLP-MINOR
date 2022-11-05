d84ec5b1a3c35bf56cc818e812c1fdf6d4d27ac5
manav <mmalhotra_be21@thapar.edu>
2022-11-05 16:18:15 +0530

    app

--- app.py
+++ app.py
@@ -1,113 +1,71 @@
-# Core Pkgs
-import streamlit as st 
+# Core packages 
+import streamlit
 import altair as alt
-import plotly.express as px 
 
-# EDA Pkgs
-import pandas as pd 
-import numpy as np 
-from datetime import datetime
+# EDA packages
+import pandas as pd
+import numpy as np
 
-# Utils
-import joblib 
+# util package 
+import joblib
+
+ 
 pipe_lr = joblib.load(open("Model_trained.pkl","rb"))
+# function
+def predict_emotion(docx):
+    results = pipe_lr.predict([docx])
+    return results[0]
 
-
-# Track Utils
-from track_utils import create_page_visited_table,add_page_visited_details,view_all_page_visited_details,add_prediction_details,view_all_prediction_details,create_emotionclf_table
-
-# Fxn
-def predict_emotions(docx):
-	results = pipe_lr.predict([docx])
-	return results[0]
-
-def get_prediction_proba(docx):
-	results = pipe_lr.predict_proba([docx])
-	return results
-
+def get_prediction_prob(docx):
+    results = pipe_lr.predict_proba([docx])
+    return results
 emotions_emoji_dict = {"anger":"😠","disgust":"🤮", "fear":"😨😱", "happy":"🤗", "joy":"😂", "neutral":"😐", "sad":"😔", "sadness":"😔", "shame":"😳", "surprise":"😮"}
 
 
-# Main Application
 def main():
-	st.title("Emotion Classifier App")
-	menu = ["Home","Monitor","About"]
-	choice = st.sidebar.selectbox("Menu",menu)
-	create_page_visited_table()
-	create_emotionclf_table()
-	if choice == "Home":
-		add_page_visited_details("Home",datetime.now())
-		st.subheader("Home-Emotion In Text")
+    streamlit.title("Emotion Classifier Engine")
+    menu = ["Home","Monitor","About"]
 
-		with st.form(key='emotion_clf_form'):
-			raw_text = st.text_area("Type Here")
-			submit_text = st.form_submit_button(label='Submit')
+    choice = streamlit.sidebar.selectbox('Menu',menu)
 
-		if submit_text:
-			col1,col2  = st.beta_columns(2)
+    if choice == "Home":
+        streamlit.subheader("Home-Emotion In Text")
 
-			# Apply Fxn Here
-			prediction = predict_emotions(raw_text)
-			probability = get_prediction_proba(raw_text)
-			
-			add_prediction_details(raw_text,prediction,np.max(probability),datetime.now())
+        with streamlit.form(key='Emotion_clf_form'):
+            raw_text = streamlit.text_area('Type your Text Here')
+            submit = streamlit.form_submit_button(label="Submit")
+        
+        if submit:
+            col1,col2 = streamlit.beta_columns(2)
+            
+            # applying the function here 
+            prediction = predict_emotion(raw_text)
+            probability = get_prediction_prob(raw_text)
+            with col1:
+                streamlit.success("Original Text")
+                streamlit.write(raw_text)
+                streamlit.success("Prediction")
+                emoji_icon = emotions_emoji_dict[prediction]
+                streamlit.write("{}:{}".format(prediction,emoji_icon))
+                streamlit.write("confidence:{}".format(np.max(probability)))           
+            
+            with col2:
+                streamlit.success("Prediction Probability ")
+                #streamlit.write(prediction)
+                proba_df = pd.DataFrame(probability,columns=pipe_lr.classes_)
+                #streamlit.write(proba_df.T)
+                proba_df_clean = proba_df.T.reset_index()
+                proba_df_clean.columns = ["Emotions", "Probability"]
 
-			with col1:
-				st.success("Original Text")
-				st.write(raw_text)
+                fig = alt.Chart(proba_df_clean).mark_bar().encode(x="Emotions",y="Probability",color="Emotions")
+                streamlit.altair_chart(fig,use_container_width=True)
 
-				st.success("Prediction")
-				emoji_icon = emotions_emoji_dict[prediction]
-				st.write("{}:{}".format(prediction,emoji_icon))
-				st.write("Confidence:{}".format(np.max(probability)))
+    elif choice == "Monitor":
+        streamlit.subheader("Monitor App")
+    
+    else:
+        streamlit.subheader("About")
+    
 
-
-
-			with col2:
-				st.success("Prediction Probability")
-				# st.write(probability)
-				proba_df = pd.DataFrame(probability,columns=pipe_lr.classes_)
-				# st.write(proba_df.T)
-				proba_df_clean = proba_df.T.reset_index()
-				proba_df_clean.columns = ["emotions","probability"]
-
-				fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions',y='probability',color='emotions')
-				st.altair_chart(fig,use_container_width=True)
-
-
-
-	elif choice == "Monitor":
-		add_page_visited_details("Monitor",datetime.now())
-		st.subheader("Monitor App")
-
-		with st.beta_expander("Page Metrics"):
-			page_visited_details = pd.DataFrame(view_all_page_visited_details(),columns=['Pagename','Time_of_Visit'])
-			st.dataframe(page_visited_details)	
-
-			pg_count = page_visited_details['Pagename'].value_counts().rename_axis('Pagename').reset_index(name='Counts')
-			c = alt.Chart(pg_count).mark_bar().encode(x='Pagename',y='Counts',color='Pagename')
-			st.altair_chart(c,use_container_width=True)	
-
-			p = px.pie(pg_count,values='Counts',names='Pagename')
-			st.plotly_chart(p,use_container_width=True)
-
-		with st.beta_expander('Emotion Classifier Metrics'):
-			df_emotions = pd.DataFrame(view_all_prediction_details(),columns=['Rawtext','Prediction','Probability','Time_of_Visit'])
-			st.dataframe(df_emotions)
-
-			prediction_count = df_emotions['Prediction'].value_counts().rename_axis('Prediction').reset_index(name='Counts')
-			pc = alt.Chart(prediction_count).mark_bar().encode(x='Prediction',y='Counts',color='Prediction')
-			st.altair_chart(pc,use_container_width=True)	
-
-
-
-	else:
-		st.subheader("About")
-		add_page_visited_details("About",datetime.now())
-
-
-
-
-
-if _name_ == '_main_':
-	main()
+if __name__ == "__main__":
+    main()
